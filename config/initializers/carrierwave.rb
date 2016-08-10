5637ae9bcf31aeb68d195db9ff99c4b5149f6682
Reginald Lucien <reginald.lucien@gmail.com>
2016-08-10 09:12:05 -0400

    added commas

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
 	config.fog_credententials = {
 		provider: 				'AWS',
-		aws_access_key_id:  	"#{ENV['AWS_ACCESS_KEY_ID']}"
-		aws_secret_key_id: 		"#{ENV['AWS_SECRET_KEY_ID']}"
-		region: 				"#{ENV['S3_REGION']}"
+		aws_access_key_id:  	"#{ENV['AWS_ACCESS_KEY_ID']}",
+		aws_secret_key_id: 		"#{ENV['AWS_SECRET_KEY_ID']}",
+		region: 				"#{ENV['S3_REGION']}",
 		path_style:  			true
 	}
 
