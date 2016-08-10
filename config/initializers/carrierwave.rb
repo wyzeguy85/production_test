191eb194f42129cb24e0cf74e9ba0953d9a2b179
Reginald Lucien <reginald.lucien@gmail.com>
2016-08-10 09:26:35 -0400

    edited key id2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,5 @@
 CarrierWave.configure do |config|
-	config.fog_credententials = {
+	config.fog_credentials = {
 		provider: 				'AWS',
 		aws_access_key_id:  	"#{ENV['AWS_ACCESS_KEY_ID']}",
 		aws_secret_access_key: 	"#{ENV['AWS_SECRET_KEY_ID']}",
